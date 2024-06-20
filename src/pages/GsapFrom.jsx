0f7bed4eb1d86b5cba65c5f40ceef08dbd0499f5
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-06-20 17:28:16 +0530

    learned about gsap.from by animating green box

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,19 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 250,
+      yoyo: true,
+      repeat: -1,
+      rotate: 360,
+      duration: 2,
+      ease: "power1.inOut",
+    });
+  }, []);
 
   return (
     <main>
